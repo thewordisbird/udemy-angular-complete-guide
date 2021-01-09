5b84d0cdb5581b2ace84db15a4f2ba55961fd80e
Justin Bird <justin@thewordisbird.dev>
2021-01-08 17:08:28 -0800

    Section 6: Course Project - Components & Databinding

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
@@ -6,13 +6,20 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test!', 'https://bitofthegoodstuff.com/wp-content/uploads/2018/05/Seitan-1-1000.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test!', 'https://bitofthegoodstuff.com/wp-content/uploads/2018/05/Seitan-1-1000.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test!', 'https://bitofthegoodstuff.com/wp-content/uploads/2018/05/Seitan-1-1000.jpg')
   ];
+
+  selectedRecipe: Recipe;
 
   constructor() { }
 
+  onRecipeSelected(recipe: Recipe){
+    console.log('Recipe Recieved at list', recipe)
+    this.recipeWasSelected.emit(recipe)
+  }
   ngOnInit(): void {
   }
 
