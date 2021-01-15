1db825044e85559ddac2ea0ff5124560cdd5119b
Justin Bird <justin@thewordisbird.dev>
2021-01-15 11:20:09 -0800

    Section 12: Course Project - Observables

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredients.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,15 +8,15 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-
+  private igChangeSub: Subscription;
   constructor(private shoppingListSercice: ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListSercice.getIngredients();
     // Subscribe to event listener
-    this.shoppingListSercice.ingredientsChanged
+    this.igChangeSub = this.shoppingListSercice.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients
@@ -23,5 +24,8 @@ export class ShoppingListComponent implements OnInit {
       )
   }
 
+  ngOnDestroy(){
+    this.igChangeSub.unsubscribe();
+  }
   
 }
