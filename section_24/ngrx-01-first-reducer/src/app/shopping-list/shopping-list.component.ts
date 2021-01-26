e7c72b7370f4f1e0f1c843c7d7ec75830e885131
Justin Bird <justin@thewordisbird.dev>
2021-01-25 21:21:17 -0800

    Section 24: NgRx first half

--- section_24/ngrx-01-first-reducer/src/app/shopping-list/shopping-list.component.ts
+++ section_24/ngrx-01-first-reducer/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from '../logging.service';
-import { Store } from '@ngrx/store';
+import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,32 +14,18 @@ import { Store } from '@ngrx/store';
 })
 export class ShoppingListComponent implements OnInit{
   ingredients: Observable<{ingredients: Ingredient[]}> ;
-  private subscription: Subscription;
 
   constructor(
-    private slService: ShoppingListService,
     private loggingService: LoggingService,
-    private store: Store<{shoppingList: { ingredients: Ingredient[]} } >
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   ngOnInit() {
     this.ingredients = this.store.select('shoppingList');
-
-    // this.ingredients = this.slService.getIngredients();
-    // this.subscription = this.slService.ingredientsChanged.subscribe(
-    //   (ingredients: Ingredient[]) => {
-    //     this.ingredients = ingredients;
-    //   }
-    // );
-
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index))
   }
-
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe();
-  // }
 }
